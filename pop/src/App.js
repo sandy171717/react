764d93b494872fa5378a0bc1463303994fbfba6c
Sandy <maksandy405@gmail.com>
2020-06-04 12:58:59 +0530

    added components

--- pop/src/App.js
+++ pop/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Greet from './components/Greet'
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+  <Greet/>
     </div>
   );
 }
---
+++ pop/src/components/Greet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Greet extends Component {
+    render() {
+        return (
+            <div>
+               <h1>This is git learning </h1> 
+            </div>
+        )
+    }
+}
+
+export default Greet
